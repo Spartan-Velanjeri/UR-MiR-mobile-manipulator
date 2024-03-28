569a7711bfff0988511b89c50afc56a557c30e1a
rudresh.lonkar <rudresh.lonkar@gmail.com>
2024-03-28 15:27:25 +0100

    Aruco to Nav2 goal

--- ros2_aruco/ros2_aruco/setup.py
+++ ros2_aruco/ros2_aruco/setup.py
@@ -25,7 +25,8 @@ setup(
     entry_points={
         'console_scripts': [
             'aruco_node = ros2_aruco.aruco_node:main',
-            'aruco_generate_marker = ros2_aruco.aruco_generate_marker:main'
+            'aruco_generate_marker = ros2_aruco.aruco_generate_marker:main',
+            'aruco_pose_to_nav_goal = ros2_aruco.aruco_pose_to_nav_goal:main',
         ],
     },
 )
